21a8c5fbc4854e0a51f27dfadfd1860ab6ef23ab
Antonio <antonioru1997@gmail.com>
2021-09-20 15:47:46 +0200

    Struttura base main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,20 @@
 <template>
   <main>
-    <h1>SEZIONE MAIN</h1>
+    <div class="main-content">
+      <h1> Content goes here </h1>
+    </div>
+
+      <div class="main-bottom">
+        <div class="bottom-content">
+          <ul>
+            <li><a href="#">Prova</a></li>
+            <li><a href="#">Prova</a></li>
+            <li><a href="#">Prova</a></li>
+            <li><a href="#">Prova</a></li>
+            <li><a href="#">Prova</a></li>
+          </ul>
+        </div>
+      </div>
   </main>
 </template>
 
@@ -13,5 +27,42 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+@import "../style/mixins";
+@import "../style/vars";
+
+
+main {
+  background-color: black;
+}
+
+.main-content {
+  width: 70%;  
+  padding: 50px 10px;
+  margin: 0 auto;
+  color: white;
+}
+
+.main-bottom {
+  background-color: $blue;
+}
+
+.bottom-content {
+  width: 70%;
+  margin: 0 auto;
+  padding: 50px;
+  @include center();
+
+  ul {
+    list-style: none;
+    @include center(vertical);
+    
+    li a {
+      margin: 30px;
+      color: white;
+      text-decoration: none;
+    }
+  }
+}
 
 </style>
