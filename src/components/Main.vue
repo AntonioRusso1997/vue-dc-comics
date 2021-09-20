d0e06440eaa95b0fe14cf7411355c8d7b846632c
Antonio <antonioru1997@gmail.com>
2021-09-21 01:40:18 +0200

    FINE CON BONUS

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,15 +3,15 @@
     <div class="main-content">
       <h1> Content goes here </h1>
     </div>
-
       <div class="main-bottom">
         <div class="bottom-content">
           <ul>
-            <li><a href="#">Prova</a></li>
-            <li><a href="#">Prova</a></li>
-            <li><a href="#">Prova</a></li>
-            <li><a href="#">Prova</a></li>
-            <li><a href="#">Prova</a></li>
+            <li v-for="(comic, index) in comics" :key="index">
+              <a :href="comic.url">
+              <img  :src="comic.img" alt="">
+              {{ comic.text }}
+              </a>
+            </li>
           </ul>
         </div>
       </div>
@@ -21,7 +21,38 @@
 <script>
 export default {
   name: 'Main',
-  
+
+  data() {
+    return {
+      comics: [
+        {
+          text: "DIGITAL COMICS",
+          img: require("../assets/img/buy-comics-digital-comics.png"),
+          url: "#"
+        },
+        {
+          text: "DC MERCHANDISE",
+          img: require("../assets/img/buy-comics-merchandise.png"),
+          url: "#"
+        },
+        {
+          text: "SUBSCIPTION",
+          img: require("../assets/img/buy-comics-subscriptions.png"),
+          url: "#"
+        },
+        {
+          text: "COMIC SHOP LOCATOR",
+          img: require("../assets/img/buy-comics-shop-locator.png"),
+          url: "#"
+        },
+        {
+          text: "DC POWER VISA",
+          img: require("../assets/img/buy-dc-power-visa.svg"),
+          url: "#"
+        }
+      ]
+    }
+  }  
 }
 </script>
 
@@ -58,9 +89,20 @@ main {
     @include center(vertical);
     
     li a {
-      margin: 30px;
+      margin-right: 60px;
       color: white;
       text-decoration: none;
+      font-size: 15px;
+      @include center();
+
+      img {
+        height: 60px;
+        margin-right: 10px; 
+      }
+    }
+
+    li:last-child a img {
+      height: 40px;
     }
   }
 }
